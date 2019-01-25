2284d449fa4c101fb371c0343f6d4e8e12e17de3
Jayson <suizo@photoup.net>
2019-01-25 12:12:14 +0800

    update

--- index.js
+++ index.js
@@ -1,36 +1,80 @@
-var express = require("express");
-var socket = require("socket.io");
+const express = require("express");
+const socket = require("socket.io");
+const mysql = require("mysql");
+const moment = require("moment");
 
 
 //App Setup
-var app = express();
+const app = express();
 
-var server = app.listen(process.env.PORT || 4000, function(){
+app.use(express.json());
+
+const connection = mysql.createConnection({
+	//properties
+	host: "192.168.9.14",
+	user: "root",
+	password: "admin",
+	database: "pu_local_server"
+
+});
+
+connection.connect(function(error) {
+	if(!!error) {
+		console.log("problem in db connection");
+	}else {
+		console.log("Connected!!");
+	}
+  
+});
+
+
+const server = app.listen(process.env.PORT || 4000, function(){
 	console.log('listen to request on ports 4000');
 });
 
-
-//Static files 
-
-app.use(express.static("public"));
-
-
 //Socket setup
-var io = socket(server);
-
+const io = socket(server);
 
 io.on("connection", function(socket){
-	console.log("made socket connection", socket.id);
-
-	socket.on("chat", function(data){
-		io.sockets.emit("chat",data);
+	var uid = null;
+	socket.on("editor_reply", function(data){
+		hid = data.peach_hid;
+		connection.query("SELECT o.uid from `orders` as o,`home` as h where o.oid = h.oid AND h.hid ="+hid+" LIMIT 1", function(error, rows, fields){
+			if(!!error) {
+				io.sockets.emit("editor_reply",{"success" : false,"data" : error});
+			} else {
+				if(rows.length > 0) {
+					io.sockets.emit("editor_reply",{"success" : true,"data" : rows,"added_data" : data});
+				}else {
+					io.sockets.emit("editor_reply",{"success" : false,"data" : "no data available!"});
+				}
+			}
+		});
+		
+	});
+	socket.on("user_reply", function(data){
+		io.sockets.emit("reply",data);
+		
 	});
 
-	socket.on("typing", function(data){
-		socket.broadcast.emit("typing",data);
+	socket.on("peachUser", function(data) {
+    	
+    });
+
+    socket.on("view_all_user", function(data) {
+    	var now = moment().format("YYYY-MM-DD:hh:mm:ss");
+    	query_string = "UPDATE `photo-note-reply` as p SET p.date_viewed = '"+now+"' WHERE p.date_viewed IS NULL AND p.sid IS NOT NULL AND p.hid IN("+data.homes+")";
+    	connection.query(query_string, function(error, rows, fields){
+			if(!!error) {
+				io.sockets.emit("view_all_user",{"success" : false,"data" : error});
+			} else {
+				io.sockets.emit("view_all_user",{"success" : true});
+			}
+		});
+
 	});
+
 });
 
 
-//https://www.youtube.com/watch?v=UwS3wJoi7fY&index=3&list=PL4cUxeGkcC9i4V-_ZVwLmOusj8YAUhj_9
 
